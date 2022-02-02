f07057ebc9a9aba2c244e01a6bdfc4ec69d96d1f
yhupaul <yhupaul@hotmail.com>
2022-02-01 21:33:06 -0800

    review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $('#tweet-text').on("input", function(e) {
+  $("textarea").on("input", function(e) {
     const content = $(this).val();
-    const counter = $(this).parent().find(".counter");
+    const counter = $(this).parent().find("output");
     counter.html(140 - content.length);
     if (content.length > 140) {
       counter.css('color','red');
@@ -9,4 +9,14 @@ $(document).ready(function() {
       counter.css('color', 'black');
     }
   });
+  // $('#tweet-text').on("input", function(e) {
+  //   const content = $(this).val();
+  //   const counter = $(this).parent().find(".counter");
+  //   counter.html(140 - content.length);
+  //   if (content.length > 140) {
+  //     counter.css('color','red');
+  //   } else {
+  //     counter.css('color', 'black');
+  //   }
+  // });
 });
